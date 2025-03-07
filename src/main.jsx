1bc270c17fa7d9cd30cb39394241648b8475ae03
chintan dankhara <chintandankhara9@gmail.com>
2025-03-07 19:10:52 +0530

    o

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { Ap } from './Ap';
+import { App } from './Ap';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
