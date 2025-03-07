bec5792c15b94efef3dfbb1d6faebe490a38ac86
chintan dankhara <chintandankhara9@gmail.com>
2025-03-07 19:00:10 +0530

    ok

--- src/also/Login.jsx
+++ src/also/Login.jsx
@@ -10,7 +10,7 @@ export const Login = () => {
         <h2 className=' text-dark-emphasis mt-5'>Login form</h2>
         <br />
         <input type="text" name="" id="" className='txt mt-3 w-75' placeholder='Username'/>
-        <input type="text" name="" id=""  className='txt mt-5 w-75' placeholder='Pasword'/> <br />
+        <input type="password" name="" id=""  className='txt mt-5 w-75' placeholder='Pasword'/> <br />
         <input type="checkbox" id='remember' className='point che me-1'/>
         <label htmlFor="remember" className='point mt-5 me-5'>Remember</label>
         <label htmlFor="foget" className='point'>Foreget password ?</label>
